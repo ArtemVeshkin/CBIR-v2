47ca14005d696c76c0219022fa9369b266aba32d
ArtemVeshkin <artem.veshkin@gmail.com>
2023-08-26 16:51:34 +0300

    autoencoder stable version

--- models/context_encoder/autoencoder.py
+++ models/context_encoder/autoencoder.py
@@ -23,76 +23,84 @@ class Autoencoder(pl.LightningModule):
 
         self.init_model(hparams)
 
-        self.val_batch_masks = []
-        self.test_batch_masks = []
-
     @staticmethod
-    def encoder_block(in_channels, out_channels, stride=2, padding=1):
-        return nn.Sequential(
-            nn.Conv2d(in_channels, in_channels, 3, padding=1, bias=False),
-            nn.BatchNorm2d(in_channels),
-            nn.LeakyReLU(True),
-            nn.Conv2d(in_channels, out_channels, 3, stride, padding, bias=False),
+    def encoder_block(in_channels, out_channels, kernel_size=3,
+                      stride=2, padding=1, n_additional_convs=0):
+        blocks = []
+        for _ in range(n_additional_convs):
+            blocks += [
+                nn.Conv2d(in_channels, in_channels, 3, padding=1, bias=False),
+                nn.BatchNorm2d(in_channels),
+                nn.LeakyReLU(True),
+            ]
+
+        blocks += [
+            nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding, bias=False),
             nn.BatchNorm2d(out_channels),
             nn.LeakyReLU(True)
-        )
+        ]
+
+        return nn.Sequential(*blocks)
 
     @staticmethod
-    def decoder_block(in_channels, out_channels, kernel_size=4, stride=2, padding=1, last_relu=True):
+    def decoder_block(in_channels, out_channels, kernel_size=3,
+                      stride=2, padding=1, n_additional_convs=0, last_relu=True):
         block = [
             nn.ConvTranspose2d(in_channels, out_channels, kernel_size, stride, padding, bias=False),
-            nn.BatchNorm2d(out_channels),
-            nn.LeakyReLU(True),
-            nn.Conv2d(out_channels, out_channels, 3, padding=1, bias=False),
             nn.BatchNorm2d(out_channels)
         ]
+
+        for _ in range(n_additional_convs):
+            block += [
+                nn.LeakyReLU(True),
+                nn.Conv2d(out_channels, out_channels, 3, padding=1, bias=False),
+                nn.BatchNorm2d(out_channels)
+            ]
+
         if last_relu:
             block.append(nn.LeakyReLU(True))
+
         return nn.Sequential(*block)
 
     def init_model(self, hparams):
-        nfe = hparams.nfe
+        nf = hparams.nf
         nz  = hparams.nz
-        nfd = hparams.nfd
+        n_additional_convs = hparams.n_additional_convs
         self.encoder = nn.Sequential(
-            # input 3 x 256 x 256
-            self.encoder_block(3, nfe),
-            # input (nfe) x 128 x 128
-            self.encoder_block(nfe, nfe * 2),
-            # input (nfe*2) x 64 x 64
-            self.encoder_block(nfe * 2, nfe * 4),
-            # input (nfe*4) x 32 x 32
-            self.encoder_block(nfe * 4, nfe * 8),
-            # input (nfe*8) x 16 x 16
-            self.encoder_block(nfe * 8, nfe * 16),
-            # input (nfe*16) x 8 x 8
-            self.encoder_block(nfe * 16, nfe * 32),
-            # input (nfe*32) x 4 x 4
-            self.encoder_block(nfe * 32, nfe * 64),
-            # input (nfe*64) x 2 x 2
-            self.encoder_block(nfe * 64, nz),
+            # input (nc) x 256 x 256
+            self.encoder_block(3, nf, 4, 2, 1),
+            # input (nf) x 128 x 128
+            self.encoder_block(nf, nf * 2, 4, 2, 1, n_additional_convs),
+            # input (nf*2) x 64 x 64
+            self.encoder_block(nf * 2, nf * 4, 4, 2, 1, n_additional_convs),
+            # input (nf*4) x 32 x 32
+            self.encoder_block(nf * 4, nf * 8, 4, 2, 1, n_additional_convs),
+            # input (nf*8) x 16 x 16
+            self.encoder_block(nf * 8, nf * 16, 4, 2, 1, n_additional_convs),
+            # input (nf*16) x 8 x 8
+            self.encoder_block(nf * 16, nf * 32, 4, 2, 1, n_additional_convs),
+            # input (nf*32) x 4 x 4
+            self.encoder_block(nf * 32, nz, 4, 1, 0),
             # output (nz) x 1 x 1
         )
 
         self.decoder = nn.Sequential(
             # input (nz) x 1 x 1
-            self.decoder_block(nz, nfd * 64, kernel_size=2, stride=1, padding=0),
-            # input (nfd*64) x 2 x 2
-            self.decoder_block(nfd * 64, nfd * 32),
-            # input (nfd*32) x 4 x 4
-            self.decoder_block(nfd * 32, nfd * 16),
-            # input (nfd*16) x 8 x 8
-            self.decoder_block(nfd * 16, nfd * 8),
-            # input (nfd*8) x 16 x 16
-            self.decoder_block(nfd * 8, nfd * 4),
-            # input (nfd*4) x 32 x 32
-            self.decoder_block(nfd * 4, nfd * 2),
-            # input (nfd*2) x 64 x 64
-            self.decoder_block(nfd * 2, nfd * 1),
-            # input (nfd) x 128 x 128
-            self.decoder_block(nfd, 3, last_relu=False),
+            self.decoder_block(nz, nf * 32, 4, 1, 0),
+            # input (nf*32) x 4 x 4
+            self.decoder_block(nf * 32, nf * 16, 4, 2, 1, n_additional_convs),
+            # input (nf*16) x 8 x 8
+            self.decoder_block(nf * 16, nf * 8, 4, 2, 1, n_additional_convs),
+            # input (nf*8) x 16 x 16
+            self.decoder_block(nf * 8, nf * 4, 4, 2, 1, n_additional_convs),
+            # input (nf*4) x 32 x 32
+            self.decoder_block(nf * 4, nf * 2, 4, 2, 1, n_additional_convs),
+            # input (nf*2) x 64 x 64
+            self.decoder_block(nf * 2, nf, 4, 2, 1, n_additional_convs),
+            # input (nf) x 128 x 128
+            self.decoder_block(nf, 3, 4, 2, 1, last_relu=False),
             nn.Tanh()
-            # output 3 x 256 x 256
+            # output (nc) x 256 x 256
         )
 
     def forward(self, x):
@@ -150,7 +158,7 @@ class Autoencoder(pl.LightningModule):
                     lr=self.hparams.lr,
                     betas=(0.9, 0.999))
 
-    def save_images(self, x, output, batch_mask, name, n=8):
+    def save_images(self, x, output, name, n=8):
         """
         Saves a plot of n images from input and output batch
         """
@@ -159,21 +167,12 @@ class Autoencoder(pl.LightningModule):
         # denormalize images
         denormalization = transforms.Normalize((-MEAN / STD).tolist(), (1.0 / STD).tolist())
         input = [denormalization(i) for i in x[:n]]
-
-        masked_input = [(denormalization(i) + (1 - mask)).clamp(0., 1.)
-                    for i, mask in zip(x[:n], batch_mask[:n])]
-
-        masked_output = [denormalization(i) * mask + denormalization(out) * (1. - mask)
-                  for i, out, mask in zip(x[:n], output[:n], batch_mask[:n])]
-
         raw_output = [denormalization(i) for i in output[:n]]
 
         # make grids and save to logger
         grid_input = vutils.make_grid(input, nrow=n)
-        grid_masked_input = vutils.make_grid(masked_input, nrow=n)
-        grid_masked_output = vutils.make_grid(masked_output, nrow=n)
         grid_raw_output = vutils.make_grid(raw_output, nrow=n)
-        grid = torch.cat((grid_input, grid_masked_input, grid_masked_output, grid_raw_output), 1)
+        grid = torch.cat((grid_input, grid_raw_output), 1)
         self.logger.experiment.add_image(name, grid, self.global_step)
 
     def generate_batch_mask(self, batch):
@@ -188,19 +187,16 @@ class Autoencoder(pl.LightningModule):
         return batch_mask
 
     def training_step(self, batch, batch_idx):
-        batch_mask = self.generate_batch_mask(batch)
-        masked_batch = batch * batch_mask
-        output = self(masked_batch)
-        loss = F.mse_loss(output * (1. - batch_mask), batch * (1. - batch_mask)) * batch_mask.mean()
+        output = self(batch)
+        loss = F.mse_loss(output, batch)
 
         # save input and output images at beginning of epoch
         if batch_idx == 0:
-            self.save_images(batch, output, batch_mask, "train_input_output")
+            self.save_images(batch, output, "train_input_output")
 
         self.logger.experiment.add_scalars('loss',
                                            {'train': loss}, self.global_step)
 
-        masked_batch.detach().cpu()
         return {
             'loss': loss
         }
@@ -210,20 +206,14 @@ class Autoencoder(pl.LightningModule):
         self.val_loss = []
 
     def validation_step(self, batch, batch_idx):
-        if len(self.val_batch_masks) < batch_idx + 1:
-            self.val_batch_masks.append(self.generate_batch_mask(batch).detach().cpu())
-        batch_mask = self.val_batch_masks[batch_idx].to(self.device)
-
-        masked_batch = batch * batch_mask
-        output = self(masked_batch)
-        loss = (F.mse_loss(output * (1. - batch_mask), batch * (1. - batch_mask)) * batch_mask.mean()).detach().cpu()
+        output = self(batch)
+        loss = (F.mse_loss(output, batch)).detach().cpu()
 
         # save input and output images at beginning of epoch
         if batch_idx == 1:
-            self.save_images(batch, output, batch_mask, "val_input_output")
+            self.save_images(batch, output, "val_input_output")
 
         self.val_loss += [loss]
-        batch_mask = batch_mask.detach().cpu()
 
     def on_validation_epoch_end(self):
         avg_loss = torch.stack(self.val_loss).mean()
@@ -235,19 +225,14 @@ class Autoencoder(pl.LightningModule):
         self.test_loss = []
 
     def test_step(self, batch, batch_idx):
-        if len(self.test_batch_masks) < batch_idx + 1:
-            self.test_batch_masks.append(self.generate_batch_mask(batch).detach().cpu())
-        batch_mask = self.test_batch_masks[batch_idx].to(self.device)
-        masked_batch = batch * batch_mask
-        output = self(masked_batch)
-        loss = (F.mse_loss(output * (1. - batch_mask), batch * (1. - batch_mask)) * batch_mask.mean()).detach().cpu()
+        output = self(batch)
+        loss = (F.mse_loss(output, batch)).detach().cpu()
 
         # save input and output images at beginning of epoch
         if batch_idx == 0:
-            self.save_images(batch, output, batch_mask, "test_input_output")
+            self.save_images(batch, output, "test_input_output")
 
         self.test_loss += [loss]
-        batch_mask = batch_mask.detach().cpu()
 
     def on_test_epoch_end(self):
         avg_loss = torch.stack(self.test_loss).mean()
--- models/context_encoder/train.py
+++ models/context_encoder/train.py
@@ -5,10 +5,16 @@ from torchsummary import summary
 
 from autoencoder import Autoencoder
 
+import os
+
 
 def main(hparams):
+    exp_name = (f"bs{hparams.batch_size}_"
+                f"nf{hparams.nf}_"
+                f"nz{hparams.nz}_"
+                f"convs_in_block_{hparams.n_additional_convs + 1}")
     logger = loggers.TensorBoardLogger(hparams.log_dir,
-                                       name=f"bs{hparams.batch_size}_nf{hparams.nfe}",
+                                       name=exp_name,
                                        default_hp_metric=False)
 
     model = Autoencoder(hparams)
@@ -21,6 +27,7 @@ def main(hparams):
                       accelerator='gpu')
     trainer.fit(model)
     trainer.test(model)
+    trainer.save_checkpoint(f"checkpoints/{exp_name}.ckpt")
 
 
 if __name__ == "__main__":
@@ -29,13 +36,13 @@ if __name__ == "__main__":
     parser.add_argument("--log_dir", type=str, default="logs", help="Logging directory")
     parser.add_argument("--num_workers", type=int, default=4, help="num_workers > 0 turns on multi-process data loading")
     parser.add_argument("--image_size", type=int, default=256, help="Spatial size of training images")
-    parser.add_argument("--max_epochs", type=int, default=500, help="Number of maximum training epochs")
-    parser.add_argument("--batch_size", type=int, default=512, help="Batch size during training")
+    parser.add_argument("--max_epochs", type=int, default=200, help="Number of maximum training epochs")
+    parser.add_argument("--batch_size", type=int, default=64, help="Batch size during training")
     parser.add_argument("--nz", type=int, default=2048, help="Size of latent vector z")
-    parser.add_argument("--nfe", type=int, default=48, help="Size of feature maps in encoder")
-    parser.add_argument("--nfd", type=int, default=48, help="Size of feature maps in decoder")
+    parser.add_argument("--nf", type=int, default=8, help="Size of feature maps in encoder/decoder")
+    parser.add_argument("--n_additional_convs", type=int, default=0, help="N additional conv layers in block")
     parser.add_argument("--lr", type=float, default=0.0003, help="Learning rate for optimizer")
-    parser.add_argument("--data_dir", type=str, default="/home/a.veshkin/data/COCO/CBIR_data/", help="Data dir")
+    parser.add_argument("--data_dir", type=str, default=f"{os.getenv('HOME')}/data/COCO/CBIR_data/", help="Data dir")
 
     args = parser.parse_args()
     main(args)
